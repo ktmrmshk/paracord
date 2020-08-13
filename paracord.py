387b48e02bd6d40e74dab697b8b1e93bd5927efb
ktmrmshk <ktmrmshk@gmail.com>
2020-08-13 19:35:03 +0900

    added pool function

--- paracord.py
+++ paracord.py
@@ -1,25 +1,49 @@
 import subprocess
 import logging
+from multiprocessing import Pool
 
-class pcord(object):
+logging.basicConfig(level=logging.DEBUG)
+
+class pwork(object):
     def __init__(self, timeout=None):
         self.timeout=timeout
-
-    def run(self, cmd):
+        
+    def work(self, cmd):
         try:
             ret=subprocess.check_output(cmd, timeout=self.timeout)
-            ('Ret: {}'.format(ret.decode('utf-8').strip() ) )
+            logging.info('Ret: {}'.format(ret.decode('utf-8').strip() ) )
         except subprocess.TimeoutExpired as e:
             logging.error('timeout')
             logging.error('{}, {}, {}, {}'.format(e.cmd, e.timeout, e.output, e.stderr))
         except:
             logging.error('some errors')
 
-def test():
+    def run(self, cmd):
+        self.work(cmd)
+
+
+class pcord(object):
+    def __init__(self, num_worker=1):
+        self.num_worker = num_worker
+
+    def run_worker(self, work, args):
+        with Pool(processes=self.num_worker) as pool:
+            pool.map(work, args)
+
+
+def test2():
     cmd = ['sleep', '2']
-    pc=pcord(1)
+    pc=pwork(1)
     pc.run(cmd)
 
+def test():
+    cmd = ['sleep', '2']
+    pc=pwork(3)
+    
+    man=pcord(2)
+
+    man.run_worker(pc.work, [cmd]*4)
+    
 
 if __name__ == '__main__':
     test()
